10ca255886ce6f396c3099085ac9552a609b4428
Mahmoud Khalil <Mahmoud Khalil>
2017-01-18 04:10:35 +0200

    add sentinel

--- config/app.php
+++ config/app.php
@@ -76,7 +76,7 @@ return [
     | to any of the locales which will be supported by the application.
     |
     */
-
+// TODO: Multi Language 1
     'locale' => 'en',
 
     /*
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
         //
 
         /*
@@ -225,6 +225,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
+
 
     ],
 
---
+++ resources/views/backend/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'codzin') }}</title>
+
+    <!-- Fonts -->
+    <link href="{{ elixir('css/app.css') }}" rel='stylesheet' type='text/css'>
+    <link href="{{ elixir('css/sweetalert2.css') }}" rel='stylesheet' type='text/css'>
+
+
+</head>
+<body>
+
+
+
+</body>
+</html>
